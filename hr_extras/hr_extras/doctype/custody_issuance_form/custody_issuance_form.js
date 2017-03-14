52e34d763e2e1571aa671f7991fe6551ac9b514c
imad abdou <igentle.appletec@gmail.com>
2017-03-14 17:17:31 +0200

    add better translations

--- hr_extras/hr_extras/doctype/custody_issuance_form/custody_issuance_form.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2016, Ebkar Technology & Management Solutions and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Custody Issuance Form', {
-	refresh: function(frm) {
-
-	}
-});
