8e872780e9aae69aa2465800c58b596e3c63532e
imad abdou <igentle.appletec@gmail.com>
2017-03-15 18:07:39 +0200

    add more doctypes

---
+++ hr_extras/config/desk.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _(""),
+			"icon": "", 
+			"items": [
+				# {
+				# 	"type": "doctype",
+				# 	"name": "ToDo",
+				# 	"label": _("To Do"),
+				# 	"description": _("Documents assigned to you and by you."),
+				# },
+			]
+		},
+	]
